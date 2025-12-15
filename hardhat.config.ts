211208d2763499e1cf4aef8237c21e46fbf60de2
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-12-15 01:30:53 +0000

    fix: added customHARDHAT_PORT to foundry.toml and hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -247,7 +247,7 @@ const config: HardhatUserConfig = {
 			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
 		},
 		local: {
-			url: `http://127.0.0.1:8545`,
+			url: `http://127.0.0.1:` + (process.env.HARDHAT_PORT ?? 8545),
 			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
 		},
 		mainnet: {
